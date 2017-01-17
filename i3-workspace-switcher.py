4604460ebb1db8fa191e046512eec8ce1de9e528
un.def <un.def@ya.ru>
2017-01-17 03:29:51 +0300

    Use custom Window class instances as history items; listen all workspace events

--- i3-workspace-switcher.py
+++ i3-workspace-switcher.py
@@ -22,13 +22,36 @@ KEY_MAPPING = {
 }
 
 
+class Workspace(object):
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __hash__(self):
+        return self.id
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    @classmethod
+    def from_container(cls, container):
+        return cls(id=container.id, name=container.name)
+
+
+class HistoryJSONEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        return obj.name
+
+
 class EventListener(object):
 
     def __init__(self, i3, history_file_path, size=None):
-        i3.on('workspace::focus', self.on_workspace_focus)
+        i3.on('workspace', self.dispatch_event)
         self.i3 = i3
         self.history_file_path = history_file_path
-        self.size = size if size is not None and size > 1 else None
+        self.size = size if isinstance(size, int) and size > 1 else None
         self.history = []
 
     def run(self):
@@ -38,19 +61,29 @@ class EventListener(object):
             pass
         self.i3.main()
 
-    def on_workspace_focus(self, i3, event):
-        if not self.history and event.old:
-            self.history.append(event.old.name)
-        workspace_name = event.current.name
+    def write_history(self):
+        with open(self.history_file_path, 'w') as history_file_obj:
+            json.dump(self.history, history_file_obj, cls=HistoryJSONEncoder)
+
+    def dispatch_event(self, connection, event):
+        event_handler = getattr(self, 'on_' + event.change, None)
+        if not event_handler:
+            return
+        current = Workspace.from_container(event.current)
+        old = Workspace.from_container(event.old) if event.old else None
+        event_handler(current, old)
+
+    def on_focus(self, current, old):
+        if not self.history and old:
+            self.history.append(old)
         try:
-            self.history.remove(workspace_name)
+            self.history.remove(current)
         except ValueError:
             pass
-        self.history.insert(0, workspace_name)
+        self.history.insert(0, current)
         if self.size and len(self.history) > self.size:
             self.history = self.history[:self.size]
-        with open(self.history_file_path, 'w') as history_file_obj:
-            json.dump(self.history, history_file_obj)
+        self.write_history()
 
 
 class GUI(object):
@@ -105,73 +138,74 @@ class GUI(object):
         self.draw()
 
 
-run_dir = os.getenv('XDG_RUNTIME_DIR')
-if run_dir is None:
-    sys.exit("env variable XDG_RUNTIME_DIR doesn't exist")
+if __name__ == '__main__':
+    run_dir = os.getenv('XDG_RUNTIME_DIR')
+    if run_dir is None:
+        sys.exit("env variable XDG_RUNTIME_DIR doesn't exist")
 
-parser = argparse.ArgumentParser(description='i3-workspace-switcher')
-parser.add_argument('-d', '--daemon',
-                    action='store_true',
-                    help='run daemon')
-parser.add_argument('-s', '--size',
-                    type=int,
-                    default=None,
-                    help='limit history size')
-parser.add_argument('-m', '--mod',
-                    default='Mod4',
-                    choices=KEY_MAPPING.keys(),
-                    help='i3 modifier key; default: Mod4 (Super)')
-parser.add_argument('-r', '--reverse',
-                    action='store_true',
-                    help='reverse order')
-args, extra_args = parser.parse_known_args()
+    parser = argparse.ArgumentParser(description='i3-workspace-switcher')
+    parser.add_argument('-d', '--daemon',
+                        action='store_true',
+                        help='run daemon')
+    parser.add_argument('-s', '--size',
+                        type=int,
+                        default=None,
+                        help='limit history size')
+    parser.add_argument('-m', '--mod',
+                        default='Mod4',
+                        choices=KEY_MAPPING.keys(),
+                        help='i3 modifier key; default: Mod4 (Super)')
+    parser.add_argument('-r', '--reverse',
+                        action='store_true',
+                        help='reverse order')
+    args, extra_args = parser.parse_known_args()
 
-history_file_path = os.path.join(run_dir, 'i3-workspace-switcher.history')
-i3 = i3ipc.Connection()
+    history_file_path = os.path.join(run_dir, 'i3-workspace-switcher.history')
+    i3 = i3ipc.Connection()
 
-if args.daemon:
-    EventListener(i3=i3, history_file_path=history_file_path,
-                  size=args.size).run()
+    if args.daemon:
+        EventListener(i3=i3, history_file_path=history_file_path,
+                      size=args.size).run()
 
-elif not os.path.exists(history_file_path):
-    sys.exit("history file doesn't exist")
+    elif not os.path.exists(history_file_path):
+        sys.exit("history file doesn't exist")
 
-else:
-    with open(history_file_path, 'r') as history_file_obj:
+    else:
+        with open(history_file_path, 'r') as history_file_obj:
+            try:
+                history = json.load(history_file_obj)
+                if not isinstance(history, list) or len(history) < 2:
+                    raise ValueError
+            except ValueError:
+                sys.exit()
+
+        pid_file_path = os.path.join(run_dir, 'i3-workspace-switcher.gui.pid')
+        mode = 'r+' if os.path.exists(pid_file_path) else 'w'
+        pid_file_obj = open(pid_file_path, mode)
         try:
-            history = json.load(history_file_obj)
-            if not isinstance(history, list) or len(history) < 2:
-                raise ValueError
-        except ValueError:
+            fcntl.lockf(pid_file_obj, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except IOError:
+            pid = int(pid_file_obj.read())
+            pid_file_obj.close()
+            os.kill(pid, signal.SIGUSR2 if args.reverse else signal.SIGUSR1)
             sys.exit()
+        pid = str(os.getpid())
+        pid_file_obj.seek(0)
+        pid_file_obj.truncate()
+        pid_file_obj.write(pid)
+        pid_file_obj.flush()
 
-    pid_file_path = os.path.join(run_dir, 'i3-workspace-switcher.gui.pid')
-    mode = 'r+' if os.path.exists(pid_file_path) else 'w'
-    pid_file_obj = open(pid_file_path, mode)
-    try:
-        fcntl.lockf(pid_file_obj, fcntl.LOCK_EX | fcntl.LOCK_NB)
-    except IOError:
-        pid = int(pid_file_obj.read())
+        gui_options_list = []
+        for item in extra_args:
+            item = item.lstrip('-')
+            if '=' in item:
+                gui_options_list.extend(item.split('=', 1))
+            else:
+                gui_options_list.append(item)
+        gui_options = dict(zip(gui_options_list[::2], gui_options_list[1::2]))
+
+        GUI(i3=i3, history=history, mod=KEY_MAPPING[args.mod],
+            reverse=args.reverse, gui_options=gui_options).run()
+
         pid_file_obj.close()
-        os.kill(pid, signal.SIGUSR2 if args.reverse else signal.SIGUSR1)
-        sys.exit()
-    pid = str(os.getpid())
-    pid_file_obj.seek(0)
-    pid_file_obj.truncate()
-    pid_file_obj.write(pid)
-    pid_file_obj.flush()
-
-    gui_options_list = []
-    for item in extra_args:
-        item = item.lstrip('-')
-        if '=' in item:
-            gui_options_list.extend(item.split('=', 1))
-        else:
-            gui_options_list.append(item)
-    gui_options = dict(zip(gui_options_list[::2], gui_options_list[1::2]))
-
-    GUI(i3=i3, history=history, mod=KEY_MAPPING[args.mod],
-        reverse=args.reverse, gui_options=gui_options).run()
-
-    pid_file_obj.close()
-    os.unlink(pid_file_path)
+        os.unlink(pid_file_path)
